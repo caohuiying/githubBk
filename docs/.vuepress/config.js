268832faf1dc5930044f691974967825293f350d
xiaobai <3242933996@qq.com>
2019-03-08 09:12:13 +0800

    3.8更新“ git commit -m 3.8更更新 git commit -m 3.8更新“

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,7 +31,7 @@ module.exports = {
                         'util',
                         'split-file-name',
                         'array-move',
-                        'Markdown'
+                        'markdown'
                     ]
                 },
                 {
